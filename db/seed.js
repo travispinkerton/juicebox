4382f715be0097b8b526920089b117102c372322
Travis Pinkerton <travispinkerton@hotmail.com>
2020-09-14 23:42:54 -0700

    day 01 wrap-up

---
+++ db/seed.js
@@ -0,0 +1,70 @@
+const {
+    client,
+    getAllUsers
+} = require('./index');
+  
+async function dropTables() {
+    try {
+      console.log("Starting to drop tables...");
+  
+      await client.query(`
+        DROP TABLE IF EXISTS users;
+      `);
+  
+      console.log("Finished dropping tables!");
+    } catch (error) {
+      console.error("Error dropping tables!");
+      throw error;
+    }
+}
+  
+async function createTables() {
+    try {
+      console.log("Starting to build tables...");
+  
+      await client.query(`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username varchar(255) UNIQUE NOT NULL,
+          password varchar(255) NOT NULL
+        );
+      `);
+  
+      console.log("Finished building tables!");
+    } catch (error) {
+      console.error("Error building tables!");
+      throw error;
+    }
+}
+  
+async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+    } catch (error) {
+      throw error;
+    }
+}
+  
+async function testDB() {
+    try {
+      console.log("Starting to test database...");
+  
+      const users = await getAllUsers();
+      console.log("getAllUsers:", users);
+  
+      console.log("Finished database tests!");
+    } catch (error) {
+      console.error("Error testing database!");
+      throw error;
+    }
+
+}
+  
+  
+rebuildDB()
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());
